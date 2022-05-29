4010a1735842c84280fcc3326b6e8e29c326131e
Lucas Nobre Barbosa <48865942+Nobre-Lucas@users.noreply.github.com>
2022-05-28 23:29:50 -0300

    major update

---
+++ curvas.py
@@ -0,0 +1,16 @@
+def pontos_bezier(grau_bezier: int, pontos_de_controle: list, t: float) -> list:
+    """
+    :param grau_bezier:
+    :param pontos_de_controle: Deve ter um número de elementos igual a grau_bezier + 1
+    :param t: Deve estar entre 0 e 1
+    :return: Um ponto único da curva, o ponto varia conforme t muda.
+    """
+    pontos = []
+    for i in range(grau_bezier):
+        pontos.append(pontos_de_controle[i])
+
+    for r in range(grau_bezier):
+        for i in range(grau_bezier - r):
+            pontos[i] = (1 - t) * pontos[i] + t * pontos[i + 1]  # interpolações lineares
+
+    return pontos[0]
